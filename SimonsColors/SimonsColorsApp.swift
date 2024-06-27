8be1cfa909e22cca6c1c5a5572f2696d45e77a17
Sam Craft <craftsam28@gmail.com>
2024-06-27 11:15:44 -0400

    Initial Commit

---
+++ SimonsColors/SimonsColorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimonsColorsApp.swift
+//  SimonsColors
+//
+//  Created by Sam Craft on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct SimonsColorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
